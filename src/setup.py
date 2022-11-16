4f3ab65e46312b82c7c9d28bd7be745bac2e273c
rohanbatrain <github@rohanbatra.in>
2022-11-16 20:11:31 +0530

    rolling back, using cookiecutter instead

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup 
-
-setup(
-    name='Second-Brain-Tools',
-    version = '0.0.1',
-    description = 'This is a module set for all the python utils that are being used on Second brain project and its subsequent Children.',
-    py_modules = ['__init__.py'],
-    package_dir ={ '' : 'src'},
-)
