d06f0978b5f1e62b300f5e501df11ac1f7e9c2f8
rohanbatrain <github@rohanbatra.in>
2022-11-18 22:23:41 +0530

    Added Default variables, setup() config()
    -> This commit takes care of setup flow of our cli app.
    -> Also config creation is managed in a much more obvious way.
    -> Not yet fully implemented, follow up commit would be there in a few
    hours.

--- src/second_brain_tools/cli.py
+++ src/second_brain_tools/cli.py
@@ -19,11 +19,13 @@ Why does this file exist, and why not put this in __main__?
 import os
 
 
+# Default Variable
+git_url = "https://github.com/rohanbatrain/Second-Brain"
+no_vault = f"""A Second Brain Vault is required in order to run this script \n You can get a copy from : {git_url}"""
 
 def main():
     """
     """
-
     setup()
     return 0
 
@@ -36,16 +38,28 @@ def setup():
     args1=input("Enter your choice, Y/N : ")
     if args1 == "Y":
       os.remove(".env")
-      Second_Brain_Directory_input = input("Please enter your Vault dir: ")
-      with open ('.env','w') as dot_env:
-        dot_env.write ("Second_Brain_Directory = " + "\"" + Second_Brain_Directory_input + "\"")
+      config()
 
     elif args1 == "N":
       print ("Nothing to do, Exiting...")
       exit()
       
   elif check == False:
-    print("Config file is missing, generating one now..")
+    print("Do you have vault folder ready locally in your system?")
+    choice_1 = input("Y/N: ")
+    if choice_1 == "Y":
+      config()
+    elif choice_1 == "N":
+      print("Do you have your vault folder in github?")
+      choice_2 = input("Y/N: ")
+      if choice_2 =="Y":
+        print ("Would you like to fetch it from the github?")
+      elif choice_2 =="N":
+        print (no_vault)
+        
+
+def config():
+    print("Config file is missing/removed. \n generating one now.. \n ")
     Second_Brain_Directory_input = input("Please enter your Vault dir: ")
     with open ('.env','w') as dot_env:
         dot_env.write ("Second_Brain_Directory = " + "\"" + Second_Brain_Directory_input + "\"")
