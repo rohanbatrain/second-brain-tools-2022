aab9740b6e0e8bf7100ad1c0ff87ee5f066df3f9
rohanbatrain <github@rohanbatra.in>
2022-11-25 20:39:56 +0530

    rolling back and hoping tox works this time

--- tests/test_second_brain_tools.py
+++ tests/test_second_brain_tools.py
@@ -1,17 +1,5 @@
-"""
-Module that holds all the test of SBT
-"""
+from second_brain_tools.cli import main
 
-from second_brain_tools.dir import ic_custom
 
-# cli.py Tests
-
-# dir.py Tests
-
-## Starting the initial check
-
-def initial_check_test():
-    "Initial check testing with 01A"
-    var = "01A"
-    var_return = "01_Capture-System/"
-    assert ic_custom(var) == var_return,  "01A returned 01_Capture-System/"
+def test_main():
+    assert main() == 0
